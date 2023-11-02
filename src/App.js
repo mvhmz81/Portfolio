4f2284c46aa8010e3b0eedd468082404ae273c8f
mvhmz81 <mvhmz@yahoo.co.uk>
2023-11-02 19:41:25 +0000

    still not eworkimg

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 //https://react-bootstrap.netlify.app/docs/layout/grid/#home//
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Carousel from "react-bootstrap/Carousel";
-import ExampleCarouselImage from "../src/components/carousel";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-//import image0.jpeg from "image0";//
 
-function ContainerExample() {
+function App() {
   return (
     <Container>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="52"
-              height="52"
-              fill="currentColor"
-              class="bi bi-house-door"
-              viewBox="0 0 52 52"
-            >
-              <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z" />
-            </svg>
-          </Navbar.Brand>
+          <Navbar.Brand href="#home"></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -40,68 +27,17 @@ function ContainerExample() {
         <Col>
           <img
             src="https://avatars.githubusercontent.com/u/110194915?v=4"
-            class="img-fluid"
+            className="img-fluid"
             alt="Responsive image"
           />
         </Col>
         <Col>
           <h1>About</h1>
           <p> As a scientist</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-youtube"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
-          </svg>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Carousel>
-            <Carousel.Item interval={1000}>
-              <ExampleCarouselImage text="First slide" />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
-                <button type="button" class="btn btn-link">
-                  Link
-                </button>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={500}>
-              <ExampleCarouselImage text="Second slide" />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <button type="button" class="btn btn-link">
-                  Link
-                </button>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <ExampleCarouselImage text="Third slide" />
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-                <button type="button" class="btn btn-link">
-                  Link
-                </button>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
         </Col>
       </Row>
     </Container>
   );
 }
 
-export default ContainerExample;
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import ContainerExample from "./thoughts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ContainerExample />
+    <App />
   </React.StrictMode>
 );
 
