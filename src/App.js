afca79d1f8fe3d709eac0daaf1113a8a6c7fbdd9
mvhmz81 <mvhmz@yahoo.co.uk>
2023-11-04 16:34:02 +0000

    added function for active links

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-//https://react-bootstrap.netlify.app/docs/layout/grid/#home//
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import logo1 from "../src/image-logos/logo1";
+import "./App.css";
+import { Navbar } from "./components/navbar";
 
 function App() {
   return (
-    <Container>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home"></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">About</Nav.Link>
-              <Nav.Link href="#link">Projects</Nav.Link>
-              <Nav.Link href="#home">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Row>
-        <Col>
-          <img
-            src="https://avatars.githubusercontent.com/u/110194915?v=4"
-            className="img-fluid"
-            alt="Responsive image"
-          />
-        </Col>
-        <Col>
-          <h1>About</h1>
-          <p> As a scientist</p>
-        </Col>
-      </Row>
-    </Container>
+    <div className="App">
+      <Navbar />
+    </div>
   );
 }
-
 export default App;
