568e4ef144a37954d75395ca70e061f6ee0f5c7a
mvhmz81 <mvhmz@yahoo.co.uk>
2023-11-07 09:36:22 +0000

    updated to imnclude profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { NavBar } from "./components/navbar";
-
+import { ProfilePic } from "./components/profile";
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ProfilePic />
     </div>
   );
 }
---
+++ src/components/profile.js
@@ -0,0 +1,28 @@
+import profilePic from "../src/image-logos/profilePic.jpg";
+
+export const ProfilePic = () => {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-3, col-sm-6 col-md-6">
+          <div class="profile-card">
+            <div class="profile-img">
+              <img src={profilePic} />
+            </div>
+            <div class="profile-content">
+              <h2 class="title">
+                Mukti Zavery
+                <span>Software Developer</span>
+              </h2>
+            </div>
+            <div class="col-xs-3, col-sm-6 col-md-6">
+              <h2 class="About">
+                Career Changer from Analytical to Teacher to developer
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
