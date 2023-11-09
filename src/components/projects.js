3c2f6126fb1dbb75fc68d917b709609d77dd392a
mvhmz81 <mvhmz@yahoo.co.uk>
2023-11-09 20:02:28 +0000

    deeloping css fro carousel

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,20 +1,26 @@
+import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Carousel } from "react-bootstrap";
 import frumo from "../image-logos/frumo.png";
 import cocktail from "../image-logos/cocktail.png";
 
 export const Projects = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
   return (
     <section className="projects" id="projects">
       <Container>
-        <Carousel>
+        <Carousel activeIndex={index} onSelect={handleSelect}>
           <Carousel.Item interval={2000}>
-            <Row>
-              <Col md={7}>
+            <Row className="justify-content-md-center">
+              <Col xs={12} md={7}>
                 <img className="d-block w-100" src={frumo} alt="first slide" />
               </Col>
-              <Col md={5}>
-                <Carousel.Caption style={{ backgroundColor: "transparent" }}>
+              <Col xs={12} md={5}>
+                <Carousel.Caption style={{ backgroundColor: "black" }}>
                   <h3>Frumo.co.uk</h3>
                   <p>
                     Get the app that makes managing your money fun and easy.
@@ -28,21 +34,22 @@ export const Projects = () => {
             </Row>
           </Carousel.Item>
           <Carousel.Item interval={2000}>
-            <Row>
-              <Col md={7}>
+            <Row className="justify-content-md-center">
+              <Col xs={12} md={7}>
                 <img
                   className="d-block w-100 h-100"
                   src={cocktail}
                   alt="Second slide"
                 />
               </Col>
-              <Col md={5}>
+              <Col xs={12} md={5}>
                 <Carousel.Caption style={{ backgroundColor: "black" }}>
                   <h3>Cocktail Builder</h3>
                   <p>
                     When in doubt utilize this app to search for cocktails by
                     name or even ingredients in the fridge
                   </p>
+                  <p>Tech Stack - React, CSS</p>
                 </Carousel.Caption>
               </Col>
             </Row>
