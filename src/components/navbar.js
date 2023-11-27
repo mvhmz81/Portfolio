65cca35cc008196c3e7b0597b6e1e06a7861b0c6
mvhmz81 <mvhmz@yahoo.co.uk>
2023-11-27 16:10:06 +0000

    added skills to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -54,15 +54,17 @@ export const NavBar = () => {
             >
               About
             </Nav.Link>
-            {/*<Nav.Link
-              href="#skills"
-              className={
-                activeLink === "skills" ? "active navbar-link" : "navbar-link"
-              }
-              onClick={() => onUpdateActiveLink("skills")}
-            >
-              Skills
-            </Nav.Link>*/}
+            {
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+            }
             <Nav.Link
               href="#projects"
               className={
